a25ad4adf5870ac9949ae4df6a2ea2dd87b75fc5
arlanbutarbutar <arlan270899@gmail.com>
2022-12-10 08:20:29 +0800

    revisi: ubah tampilan profile sekolah pada halaman index ke halaman profil tersendiri. Buat relasi antara guru dan jadwal

--- controller/db_connect.php
+++ controller/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "sman_2_kupang_timur2");
+$conn = mysqli_connect("localhost", "root", "", "sman_2_kupang_timur");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
